00dcf805b55f5870e8c6f0133c2ee5e5153232d8
Your Name <you@example.com>
2024-09-28 21:35:50 +0200

    removing 2 projects from section works

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,148 +1,100 @@
-import React from 'react'
+import React from "react";
 
 function Skills() {
   return (
     <section id="skills" className="skills bg-dark">
-    <h1 className="skills__main-title">SKILLS</h1>
-    <div className="skills__container">
+      <h1 className="skills__main-title">SKILLS</h1>
+      <div className="skills__container">
         <div className="skills__info">
-            <h1 className="skills__title title ">Technical Skills </h1>
-            <div className="skills__desc">
-                
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>HTML5</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>CSS3</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>TailwindCSS</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Bootstrap</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>React.js</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Angular.js</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>GIT</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Postman</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Figma</p>
-                </div>
-                <div className="desc mx-auto"> 
-                    <span className="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Photoshop</p>
-                </div>
+          <h1 className="skills__title title ">Technical Skills </h1>
+          <div className="skills__desc">
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>HTML5</p>
             </div>
-      
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>CSS3</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>TailwindCSS</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>Bootstrap</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>React.js</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>Angular.js</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>GIT</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>Postman</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>Figma</p>
+            </div>
+            <div className="desc mx-auto">
+              <span className="material-symbols-outlined">new_releases</span>
+              <p>Photoshop</p>
+            </div>
+          </div>
         </div>
         <div class="skills__info">
-            <h1 class="skills__title title ">Professional Skills</h1>
-            <div class="skills__desc">
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Responsible
-                    </p>
-                </div>
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Diligent</p>
-                </div>
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Honest</p>
-                </div>
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Flexability</p>
-                </div>
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Punctuality</p>
-                </div>
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Creativity</p>
-                </div>
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Teamwork</p>
-                </div>
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Communication</p>
-                </div>
-
-                <div class="desc mx-auto"> 
-                    <span class="material-symbols-outlined">
-                        new_releases
-                        </span>
-                    <p>Problem Solving</p>
-                </div>
-              
-               
+          <h1 class="skills__title title ">Professional Skills</h1>
+          <div class="skills__desc">
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Responsible</p>
             </div>
-      
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Diligent</p>
+            </div>
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Honest</p>
+            </div>
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Flexability</p>
+            </div>
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Punctuality</p>
+            </div>
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Creativity</p>
+            </div>
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Teamwork</p>
+            </div>
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Communication</p>
+            </div>
+
+            <div class="desc mx-auto">
+              <span class="material-symbols-outlined">new_releases</span>
+              <p>Problem Solving</p>
+            </div>
+          </div>
         </div>
-       
-      
-        </div>
-  
-    
-</section>
-  )
+      </div>
+    </section>
+  );
 }
 
-export default Skills
+export default Skills;
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,45 +1,70 @@
-import React from 'react'
+import React from "react";
 
 function Works() {
-    return (
-        <section className="works" id="works">
-            <h1 className="works__main-title">Works</h1>
-            <div className="works__container">
-                <div className="works__list">
-                    <div className="works__item">
-                        <div className="works__image">
-                            <a href="https://diagnostic-centerrrrrrr.netlify.app/"><img src="https://alokamedicare.in/wp-content/uploads/2022/01/Factors-to-be-considered-while-choosing-the-Best-Diagnostic-Center.jpeg" alt="Video portal"/></a>
-                        </div>
-                        <div className="works__desc">Diagnostic Center</div>
-                    </div>
-                    <div className="works__item">
-                        <div className="works__image">
-                            <a href="https://digitalagencyyyyyyy.netlify.app/"><img src="https://teamtweaks1-blog.s3.us-east-2.amazonaws.com/blog/wp-content/uploads/2023/09/05133724/Best-Digital-Marketing-Agencies-In-Mumbai.jpg" alt="Video portal"/></a>
-                        </div>
-                        <div className="works__desc">Digital Agency</div>
-                    </div>
-                    <div className="works__item">
-                        <div className="works__image">
-                            <a href="https://virtual-realityyyyyyy.netlify.app/"><img src="https://www.smu.edu/-/media/Site/Meadows/NewsStories/2023/Bisk-SMU-New-Masters-Program/Creative-Technology-girl-wearing-VR-Goggles.png" alt="Video portal"/></a>
-                        </div>
-                        <div className="works__desc">Virtual Reality</div>
-                    </div>
-                    <div className="works__item">
-                        <div className="works__image">
-                            <a href="https://github.com/anfoot01/PlantShop-Teamwork"><img src="https://asaipa.co.za/wp-content/uploads/2016/11/blog-1-2.png" alt="Video portal"/></a>
-                        </div>
-                        <div className="works__desc">Organic Shop</div>
-                    </div>
-                    <div className="works__item">
-                        <div className="works__image">
-                            <a href="https://wweeaatthheerrraaappp.netlify.app/"><img src="https://images.twinkl.co.uk/tw1n/image/private/t_630/u/ux/wolfgang-hasselmann-br-gllg7bs-unsplash-2_ver_1.jpg" alt="Video portal"/></a>
-                        </div>
-                        <div className="works__desc">Weather App</div>
-                    </div>
-                </div>
+  return (
+    <section className="works" id="works">
+      <h1 className="works__main-title">Works</h1>
+      <div className="works__container">
+        <div className="works__list">
+          <div className="works__item">
+            <div className="works__image">
+              <a href="https://diagnostic-centerrrrrrr.netlify.app/">
+                <img
+                  src="https://alokamedicare.in/wp-content/uploads/2022/01/Factors-to-be-considered-while-choosing-the-Best-Diagnostic-Center.jpeg"
+                  alt="Video portal"
+                />
+              </a>
             </div>
-        </section>
-    )
+            <div className="works__desc">Diagnostic Center</div>
+          </div>
+          <div className="works__item">
+            <div className="works__image">
+              <a href="https://digitalagencyyyyyyy.netlify.app/">
+                <img
+                  src="https://teamtweaks1-blog.s3.us-east-2.amazonaws.com/blog/wp-content/uploads/2023/09/05133724/Best-Digital-Marketing-Agencies-In-Mumbai.jpg"
+                  alt="Video portal"
+                />
+              </a>
+            </div>
+            <div className="works__desc">Digital Agency</div>
+          </div>
+          <div className="works__item">
+            <div className="works__image">
+              <a href="https://virtual-realityyyyyyy.netlify.app/">
+                <img
+                  src="https://www.smu.edu/-/media/Site/Meadows/NewsStories/2023/Bisk-SMU-New-Masters-Program/Creative-Technology-girl-wearing-VR-Goggles.png"
+                  alt="Video portal"
+                />
+              </a>
+            </div>
+            <div className="works__desc">Virtual Reality</div>
+          </div>
+          <div className="works__item">
+            <div className="works__image">
+              <a href="https://github.com/anfoot01/PlantShop-Teamwork">
+                <img
+                  src="https://asaipa.co.za/wp-content/uploads/2016/11/blog-1-2.png"
+                  alt="Video portal"
+                />
+              </a>
+            </div>
+            <div className="works__desc">Organic Shop</div>
+          </div>
+          <div className="works__item">
+            <div className="works__image">
+              <a href="https://wweeaatthheerrraaappp.netlify.app/">
+                <img
+                  src="https://images.twinkl.co.uk/tw1n/image/private/t_630/u/ux/wolfgang-hasselmann-br-gllg7bs-unsplash-2_ver_1.jpg"
+                  alt="Video portal"
+                />
+              </a>
+            </div>
+            <div className="works__desc">Weather App</div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Works
+export default Works;
